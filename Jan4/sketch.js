be990d75f28b1ae69554a11058b78471fb9ead63
One_True <60509431+onetrue-6657@users.noreply.github.com>
2025-01-04 20:32:07 -0500

    Jan 4

--- Jan4/sketch.js
+++ Jan4/sketch.js
@@ -1,7 +1,20 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+  angleMode(DEGREES);
 }
 
 function draw() {
-  background(220);
+  background("#000000");
+
+  push();
+  rectMode(CENTER);
+  translate(width / 2, height / 2);
+  for (let i = 60, j = 1000; j > 0.01; i -= 1) {
+    fill(10 - i * 0.08, 10 - i * 0.08, 10 - i * 0.08, 255 - i * 4);
+    stroke(15 + i * 0.02, 15 + i * 0.02, 30 + i * 0.02);
+    rotate(360 / 50);
+    rect(0, 0, j, j);
+    j -= 15;
+  }
+  pop();  
 }
